bcb1b50a9f4fec816abe2faabf2e33b6d03a5174
Bonagiri Charan <bonagiricharan8517@gmail.com>
2026-06-19 15:18:07 +0530

    Started working  Hibernate CRUD operations for Employee entity

--- Hibernate/Project2/employee/src/main/java/com/example/employee/Employee.java
+++ Hibernate/Project2/employee/src/main/java/com/example/employee/Employee.java
@@ -1,5 +1,47 @@
 package com.example.employee;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Employee {
+	@Id
+	private int employee_id;
+	private String employe_name;
+	private String department;
+	private double salary;
+	private Date joiningDate;
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public void setEmploye_name(String employe_name) {
+		this.employe_name = employe_name;
+	}
+	public void setEmployee_id(int employee_id) {
+		this.employee_id = employee_id;
+	}
+	public void setJoiningDate(Date joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public String getEmploye_name() {
+		return employe_name;
+	}
+	public int getEmployee_id() {
+		return employee_id;
+	}
+	public Date getJoiningDate() {
+		return joiningDate;
+	}
+	public double getSalary() {
+		return salary;
+	}
 
 }
--- Hibernate/Project2/employee/src/main/java/com/example/employee/Utility.java
+++ Hibernate/Project2/employee/src/main/java/com/example/employee/Utility.java
@@ -1,5 +1,25 @@
 package com.example.employee;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Utility {
 
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration()
+                    .configure("hibernate.cfg.xml")
+                    .addAnnotatedClass(Employee.class)
+                    .buildSessionFactory();
+        }
+
+        return sessionFactory;
+    }
+    public static void shutdown() {
+    	sessionFactory.close();
+    }
+    
 }
